17943e855aeb3a345c6756735347456e156ecfd2
Riya Sen <missriyasen294@gmail.com>
2026-02-25 10:25:59 +0600

    Initial React blog project setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,65 +1,19 @@
-import ToDo from "./ToDo";
-import Login ,{User,Setting,Userkey} from "./UserComponent";
-
-
+import { useState } from "react";
+import Counter from "./Counter";
 function App(){
-  const userobj={ name:"john", age:30, city:"bangalore"};
-  const userarray=["john","doe","smith"];
-  let path="E:/react-blog/public/vite.svg";
-
-  const name=undefined;
-  function operation(a,b,op){
-
-    if (op==="+"){
-      return a+b;
-    }else if(op==="-"){
-      return a-b;
-    }else if(op==="*"){
-      return a*b;
-    }else if(op==="/"){
-      return a/b;
-    }
+  let [fruit, setFruit] = useState("Apple");
+  const handlefruit = () => {
+    setFruit("Banana");
   }
-
-  const address="bangalore"
-  function add(a,b){
-   return (a+b);  
-  }
-  // const name="react";
-  let x=20;
-  let y=30;
-  return(
-    <div>first project
-      {/* {name} */}
-      {10+20}
-      {x*y}
-      <Login/>
-      <User/>
-      <Setting/>
-      <Fruit/>  
-      <ToDo/>
-      <h1>{Userkey}</h1>
-      <button onClick={()=>alert()}>click</button>
-       <input type="text" value={name} />
-      <h1>{address}</h1>
-     <h1>{x+y}</h1>
-      <h1>{add(10,10)}</h1>
-      <h1>{operation(10,20,"+")}</h1>
-      <h1>{name?name:"default"}</h1>
-      <h1>{userobj.age}</h1>
-      <h1>{userarray[0]}</h1>
-      <h1>{path}</h1> 
-      
-
-     </div> 
-    
-  )
-}
-
-function Fruit(){
   return (
-    <div>fruit</div>
+    <div>
+      <h1>My React App</h1>
+      <p>My favorite fruit is: {fruit}</p>
+      <button onClick={handlefruit}>Change Fruit</button>
+      <h1><Counter /></h1>
+    </div>
   )
+  
+  
 }
-
 export default App;
---
+++ src/Counter.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+const Counter = () => {
+    let [count, setCount] = useState(0);
+    return(
+        <div>
+            <p>Count: {count}</p>
+            <button onClick={() => setCount(count + 1)}>Increment</button>
+        </div>
+    )
+}
+export default Counter;
